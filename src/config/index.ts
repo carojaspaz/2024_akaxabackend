f0d8ba82e7e8461a0ed7cbb7f0f34d6072ba4dc2
Carlos Andrés Rojas Paz <carojaspaz@gmail.com>
2024-12-11 20:59:28 +0000

    Release

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,14 +5,14 @@ import { ApiConfig } from '../core/constants';
 
 const production = process.env.NODE_ENV || ApiConfig.setup.productionEnv;
 const version = 1;
-const urlDatabase = process.env.DATA_DB_URL || "mongodb+srv://developer:eD3Big5TuFNEsJAF@akaxadevelopment.uddpu.azure.mongodb.net/akaxadata?retryWrites=true&w=majority";
-const jwtSecret = process.env.JWT_SECRET || "l36wo6zrSraxGsd5AITXyPhfy9m5Mxio9z4tMiNc";
-const port = process.env.PORT || 5000;
+const urlDatabase = process.env.DATA_DB_URL;
+const jwtSecret = process.env.JWT_SECRET;
+const port = process.env.PORT;
+const sendGrid = process.env.SENDGRID;
 const awsBucket = process.env.AWS_BUCKET || "akaxabucket"
 const awsId = process.env.AWS_ID || "AKIAYNQVSAE2QFV7TIOQ"
 const awsSecret = process.env.AWS_SECRET || "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"
 const awsRegion = process.env.AWS_REGION || 'us-east-2'
-const sendGrid = process.env.SENDGRID || "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"
 const email = process.env.EMAIL
 const superUserEmail = 'superuser@safetyntrust.com'
 
